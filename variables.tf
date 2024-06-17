aba78731a0a3e994ad907cb4c5f0f18373cf3334
dongrep <dongre.p@northeastern.edu>
2024-06-17 17:07:16 -0400

    feat: aws infra to launch eks cluster

---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "profile" {
+  type    = string
+  default = "infra"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  type    = string
+  default = "10.0.0.0/24"
+}
+
+variable "availability_zone" {
+  type    = string
+  default = "us-east-1a"
+}
+
+variable "default_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "protocol" {
+  type    = string
+  default = "tcp"
+}
+
+variable "http_port" {
+  type    = number
+  default = 80
+}
+
+variable "https_port" {
+  type    = number
+  default = 443
+}
+
+variable "PUBLIC_SUBNETS" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "PRIVATE_SUBNETS" {
+  type    = list(string)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
